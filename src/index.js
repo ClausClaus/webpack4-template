eccdcc3a96f196990959eb106d4fef957c1f78be
ClausClaus <weixinla84@gmail.com>
2020-09-06 21:15:58 +0800

    模块懒加载

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import './index.css';
 import './index.less';
 import _ from 'lodash';
 
+// 懒加载支持
+setTimeout(() => {
+  import('./common/js/dynamic-data').then((res) => {
+    console.log(res.default.message);
+  });
+}, 1500);
+
 const arr = [];
 console.log('调用第三方模块方法 _.isArray(arr) :>> ', _.isArray(arr));
 
